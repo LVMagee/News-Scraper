6fa1c7cf2549598d467c76460397766e859e81a6
LV Magee <lvmagee@gmail.com>
2017-06-26 11:55:09 -0400

    Added note.js

--- models/Article.js
+++ models/Article.js
@@ -20,6 +20,6 @@ var ArticleSchema = new Schema({
 	}
 });
 
-var Article - mongoose.model('Article', ArticleSchema);
+var Article = mongoose.model('Article', ArticleSchema);
 
 module.exports = Article; 
